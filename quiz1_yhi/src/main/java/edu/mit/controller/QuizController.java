2ef5837fa10eafa80836efa90783a7c3b5fbbe8e
yang <yang@yang>
2023-09-22 16:52:04 +0900

    9. 이어서 사용자가 2개의 숫자를 입력해서 전송하면
    /quiz/result.jsp 화면이 열리고 여기에는 두개의 숫자를 모두 더해서 보내온값을 출력하는 화면
    (단, 두개의 숫자를 모두더하는 작업은 MyLogic3 클래스를 이용해서 처리할것)

--- quiz1_yhi/src/main/java/edu/mit/controller/QuizController.java
+++ quiz1_yhi/src/main/java/edu/mit/controller/QuizController.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import edu.mit.dao.TwoVO;
+import edu.mit.service.QuizService;
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
+@AllArgsConstructor
 public class QuizController {
+	
+	private QuizService service;
 
 	@GetMapping("/quiz/quiz1")
 	public void quiz(Model model) {
@@ -23,8 +28,11 @@ public class QuizController {
 	}
 	
 	@PostMapping("/quiz/quiz2")
-	public void quiz2(TwoVO vo) {
+	public String quiz2(TwoVO vo,Model model) {
 		log.info("수집된 내용:"+vo);
+		
+		model.addAttribute("sum",service.allsum(vo));
+		return "/quiz/result";
 	}
 }
 
---
+++ quiz1_yhi/src/main/java/edu/mit/service/QuizService.java
@@ -0,0 +1,21 @@
+package edu.mit.service;
+
+
+
+import org.springframework.stereotype.Service;
+
+import edu.mit.dao.TwoVO;
+import edu.mit.logic.MyLogic3;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class QuizService {
+		
+	private MyLogic3 mylogic;
+	
+	//두수를 모두 더해서 돌려주는 서비스
+	public int allsum(TwoVO vo ) {
+		return mylogic.addUp(vo.getNum1(), vo.getNum2());
+	}
+}
